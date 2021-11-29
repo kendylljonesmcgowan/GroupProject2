bfaeee0dbf64148bf351997671a91c71859b3590
kendylljonesmcgowan <kendyll.jones-mcgowan@flemingcollege.ca>
2021-11-29 15:01:42 -0500

    Kendyll Code v1

---
+++ Kendyll.py
@@ -0,0 +1,19 @@
+## File: PSP Assignment 2, Implementation
+## Find the PURRfect Friend Game
+## Carol Buckingham, Kendyll Jones-McGowan, Rachel Manderfeld, Vince Ruel Alonte, GEOM 67 Section 61 
+## Last modified November 29th, 2021
+## Inputs: Player Name, Interaction Action, Adoption Selection, 
+## Outputs: Purr Points, Lives Lost, 
+
+print("Welcome to the find your PURRfect friend game!")
+
+playername = input(str("What is your name?")
+
+print( "Hello," + str(playername) + "Welcome to Meow York City!! We will go thru 4 different Cat Discovery Areas. Where we will get to know the Unique Personalities of the different Cats To become the PURfect Cat Owner!" )
+print( "The goal of the game is to adopt all of the 16 cats. Each correct action towards a cat earns you “Purr Points”. The more Purr Points you earn the higher probability to adopt the cat. You have nine lives and you will lose a life and 3 “Purr Points” if the cat chooses not to be adopted." )
+print( "CATch them all! Hope you have PAWsome time playing our game!" )
+
+input(str("Which location would you like to visit first? PAWrk,"))
+
+
+

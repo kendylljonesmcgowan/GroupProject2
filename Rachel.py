487e4ca5c0c2300c59d9783ffe2f1604dc077a71
MoonRaySpex <rachel@myemail.com>
2021-11-29 14:52:33 -0500

    push to git

---
+++ Rachel.py
@@ -0,0 +1,47 @@
+import math
+
+#define functions
+
+
+#call in CSV
+#open file catdata.csv for reading
+with open('catdata.csv') as catdata:
+    for cat in catdata:
+
+
+
+
+def main():
+
+    # Display program purpose
+    print("Welcome to 'PURRfect Pals'")
+    print("Get ready to meet and adopt feline friends \n from four different cat rescues.")
+    print("Each cat is looking for their furrever home, will you be the purrfect match?")
+    print("")
+    print('************************************************************************************************') 
+    print()
+
+#################### Start of input ########################
+
+
+
+
+
+
+
+
+############### End of input, Start of calculations ################
+
+
+
+
+
+############## End of calculations, Start of output #################
+
+
+
+
+
+
+if __name__=='__main__':  #calling defined function 'main'
+    main()

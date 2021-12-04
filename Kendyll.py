a610f4d3d857cbbbdcc18b5869020a37e8f9e349
kendylljonesmcgowan <kendyll.jones-mcgowan@flemingcollege.ca>
2021-12-04 17:20:39 -0500

    Kendyll Cat Action Code

--- Kendyll.py
+++ Kendyll.py
@@ -5,60 +5,220 @@
 ## Inputs: Player Name, Interaction Action, Adoption Selection, 
 ## Outputs: Purr Points, Lives Lost, 
 
-## Kendyll's Cat Intro's and Reactions ##
-
-## CatName,Location,LocAlias,Colour,Breed,Personality,Age,      Brushing,Pets,OtherCats,Playing,Treats
-
-        ## Yams,B,Kitten City Rescue,Orange,Short-Hair,athletic,kitten,n,y,y,y,y
-
-print("Meow, my name is Yams! I am an orange Short-Hair with lots of energy. I love pets, playng and yummy treats...")
-
-print("Meow, I didnt like that, I dont think this is going to work...")
-## add ascii cat walking away
-
-print("Meow, I liked that, I think this is going to work out!")
-## add happy ascii cat
-
-        ## Leaf,B,Kitten City Rescue,Black,Short-Hair,aloof,adult,y,n,n,n,y
-
-print("Meow, my name is Leaf! I am a black Short-Hair and like to act aloof. I love brushing and yummy treats...")
-
-print("Meow, I didnt like that, I dont think this is going to work...")
-## add ascii cat walking away
-
-print("Meow, I liked that, I think this is going to work out!")
-## add happy ascii cat
-
-        ## Fox,B,Kitten City Rescue,Orange-White,Short-Hair,curious,adult,n,y,y,y,y
-
-print("Meow, my name is Fox! I am an orange-white Short-Hairfull of curosity. I love pets, playng and yummy treats...")
-
-print("Meow, I didnt like that, I dont think this is going to work...")
-## add ascii cat walking away
-
-print("Meow, I liked that, I think this is going to work out!")
-## add happy ascii cat
-
-        ## Dana,B,Kitten City Rescue,Calico,Short-Hair,independent,adult,y,y,n,n,n
-
-print("Meow, my name is Dana! I am a calico Short-Hair that is very independent. I love brushing...")
-
-print("Meow, I didnt like that, I dont think this is going to work...")
-## add ascii cat walking away
-
-print("Meow, I liked that, I think this is going to work out!")
-## add happy ascii cat
-
-## Game Start ##
-
-print("Welcome to the find your PURRfect friend game!")
-
-playername = input(str("What is your name?"))
-
-print( "Hello," + playername + "Welcome to Meow York City!! We will go thru 4 different Cat Discovery Areas. Where we will get to know the Unique Personalities of the different Cats To become the PURfect Cat Owner!" )
-print( "The goal of the game is to adopt all of the 16 cats. Each correct action towards a cat earns you “Purr Points”. The more Purr Points you earn the higher probability to adopt the cat. You have nine lives and you will lose a life and 3 “Purr Points” if the cat chooses not to be adopted." )
-print( "CATch them all! Hope you have PAWsome time playing our game!" )
-
-input(str("Which location would you like to visit first? PAWrk,"))
+import random
 
 
+def main(username):
+        print("Hello" + username)
+        # all code goes below
+
+        ## call in csv
+
+        ## Location Code, begin Location Loop ##
+
+        Location = "Kitten City Rescue"
+        print("Meow-come to Kitten City Rescue! We have so many purrrfect cats here, and all of them can't wait to meet you! We hope you have a blast getting to know each of our paw-some friends! Enjoy," + username "and stay as long as you would like!"))
+
+        # for index in Location
+
+        ## Cat Intro's and Reactions ##
+
+purrpoints = int(0)
+
+        ## CatName,Location,LocAlias,Colour,Breed,Personality,Age,      Brushing,Pets,OtherCats,Playing,Treats
+
+        # if catname = "Yams"
+                ## Yams,B,Kitten City Rescue,Orange,Short-Hair,athletic,kitten,n,y,y,y,y
+
+                print("Meow, my name is Yams! I am an orange Short-Hair kitten with lots of energy. Lets go on an adventure outside and get lost! It is so much fun to sneak out of the house. I love pets, playng and yummy treats...")
+
+                # if brushaction = Y
+                        print("Meow, I didnt like that, I dont think this is going to work...")
+                        print("No Purr Points earned")
+                        ## add ascii cat walking away
+                # else
+                print("Meow, you seem like a lot of fun, I think this is going to work out!")
+                print("I would love for you to adopt me!")
+                purrpoints = (purrpoints + 3)
+                ## add happy ascii cat
+
+                # if petaction = N
+                        print("Meow, I didnt like that, I dont think this is going to work...")
+                        print("No Purr Points earned")
+                        ## add ascii cat walking away
+                # else
+                        print("Meow, you seem like a lot of fun, I think this is going to work out!")
+                        print("I would love for you to adopt me!")
+                        purrpoints = (purrpoints + 3)
+                        ## add happy ascii cat
+
+                # if playaction = N
+                        print("Meow, I didnt like that, I dont think this is going to work...")
+                        print("No Purr Points earned")
+                        ## add ascii cat walking away
+                # else
+                        print("Meow, you seem like a lot of fun, I think this is going to work out!")
+                        print("I would love for you to adopt me!")
+                        purrpoints = (purrpoints + 3)
+                        ## add happy ascii cat
+
+                # if treataction = N
+                        print("Meow, I didnt like that, I dont think this is going to work...")
+                        print("No Purr Points earned")
+                        ## add ascii cat walking away
+                # else
+                        print("Meow, you seem like a lot of fun, I think this is going to work out!")
+                        print("I would love for you to adopt me!")
+                        purrpoints = (purrpoints + 3)
+                        ## add happy ascii cat        
+
+        # if catname = "Leaf"
+                        ## Leaf,B,Kitten City Rescue,Black,Short-Hair,aloof,adult,y,n,n,n,y
+
+                print("Meow, my name is Leaf! I am a black Short-Hair adult and like to act aloof. I am happiest when napping in the corner while you read on a cozy winters day. I love brushing and yummy treats...")
+
+                # if brushaction = N
+                        print("Meow, I didnt like that, I dont think this is going to work...")
+                        print("No Purr Points earned")
+                        ## add ascii cat walking away
+                # else
+                print("Meow, you seem like a lot of fun, I think this is going to work out!")
+                print("I would love for you to adopt me!")
+                purrpoints = (purrpoints + 3)
+                ## add happy ascii cat
+
+                # if petaction = Y
+                        print("Meow, I didnt like that, I dont think this is going to work...")
+                        print("No Purr Points earned")
+                        ## add ascii cat walking away
+                # else
+                        print("Meow, you seem like a lot of fun, I think this is going to work out!")
+                        print("I would love for you to adopt me!")
+                        purrpoints = (purrpoints + 3)
+                        ## add happy ascii cat
+
+                # if playaction = Y
+                        print("Meow, I didnt like that, I dont think this is going to work...")
+                        print("No Purr Points earned")
+                        ## add ascii cat walking away
+                # else
+                        print("Meow, you seem like a lot of fun, I think this is going to work out!")
+                        print("I would love for you to adopt me!")
+                        purrpoints = (purrpoints + 3)
+                        ## add happy ascii cat
+
+                # if treataction = N
+                        print("Meow, I didnt like that, I dont think this is going to work...")
+                        print("No Purr Points earned")
+                        ## add ascii cat walking away
+                # else
+                        print("Meow, you seem like a lot of fun, I think this is going to work out!")
+                        print("I would love for you to adopt me!")
+                        purrpoints = (purrpoints + 3)
+                        ## add happy ascii cat
+
+                # if catname = "Fox"
+                        ## Fox,B,Kitten City Rescue,Orange-White,Short-Hair,curious,adult,n,y,y,y,y
+
+                print("Meow, my name is Fox! I am an orange-white Short-Hair adult that's full of curosity. I like to stare out the window and watch birds all day. I love pets, playng and yummy treats...")
+
+                # if brushaction = Y
+                        print("Meow, I didnt like that, I dont think this is going to work...")
+                        print("No Purr Points earned")
+                        ## add ascii cat walking away
+                # else
+                print("Meow, you seem like a lot of fun, I think this is going to work out!")
+                print("I would love for you to adopt me!")
+                purrpoints = (purrpoints + 3)
+                ## add happy ascii cat
+
+                # if petaction = N
+                        print("Meow, I didnt like that, I dont think this is going to work...")
+                        print("No Purr Points earned")
+                        ## add ascii cat walking away
+                # else
+                        print("Meow, you seem like a lot of fun, I think this is going to work out!")
+                        print("I would love for you to adopt me!")
+                        purrpoints = (purrpoints + 3)
+                        ## add happy ascii cat
+
+                # if playaction = N
+                        print("Meow, I didnt like that, I dont think this is going to work...")
+                        print("No Purr Points earned")
+                        ## add ascii cat walking away
+                # else
+                        print("Meow, you seem like a lot of fun, I think this is going to work out!")
+                        print("I would love for you to adopt me!")
+                        purrpoints = (purrpoints + 3)
+                        ## add happy ascii cat
+
+                # if treataction = N
+                        print("Meow, I didnt like that, I dont think this is going to work...")
+                        print("No Purr Points earned")
+                        ## add ascii cat walking away
+                # else
+                        print("Meow, you seem like a lot of fun, I think this is going to work out!")
+                        print("I would love for you to adopt me!")
+                        purrpoints = (purrpoints + 3)
+                        ## add happy ascii cat
+
+                # if catname = "Dana"
+                        ## Dana,B,Kitten City Rescue,Calico,Short-Hair,independent,adult,y,y,n,n,n
+
+                print("Meow, my name is Dana! I am a calico Short-Hair adult that is very independent. My dream day includes sleeping all day and running around the house late at night. I love brushing...")
+
+                # if brushaction = N
+                        print("Meow, I didnt like that, I dont think this is going to work...")
+                        print("No Purr Points earned")
+                        ## add ascii cat walking away
+                # else
+                print("Meow, you seem like a lot of fun, I think this is going to work out!")
+                print("I would love for you to adopt me!")
+                purrpoints = (purrpoints + 3)
+                ## add happy ascii cat
+
+                # if petaction = N
+                        print("Meow, I didnt like that, I dont think this is going to work...")
+                        print("No Purr Points earned")
+                        ## add ascii cat walking away
+                # else
+                        print("Meow, you seem like a lot of fun, I think this is going to work out!")
+                        print("I would love for you to adopt me!")
+                        purrpoints = (purrpoints + 3)
+                        ## add happy ascii cat
+
+                # if playaction = Y
+                        print("Meow, I didnt like that, I dont think this is going to work...")
+                        print("No Purr Points earned")
+                        ## add ascii cat walking away
+                # else
+                        print("Meow, you seem like a lot of fun, I think this is going to work out!")
+                        print("I would love for you to adopt me!")
+                        purrpoints = (purrpoints + 3)
+                        ## add happy ascii cat
+
+                # if treataction = Y
+                        print("Meow, I didnt like that, I dont think this is going to work...")
+                        print("No Purr Points earned")
+                        ## add ascii cat walking away
+                # else
+                        print("Meow, you seem like a lot of fun, I think this is going to work out!")
+                        print("I would love for you to adopt me!")
+                        purrpoints = (purrpoints + 3)
+                        ## add happy ascii cat
+
+
+
+if __name__=='__main__':        # calling defined function 'main'
+        username = input("What is your name?")
+        main(username)
+
+## Old code to save just in case ##
+
+# print("Welcome to the find your PURRfect friend game!")
+
+# playername = input(str("What is your name?"))
+
+# print( "Hello," + playername + "Welcome to Meow York City!! We will go thru 4 different Cat Discovery Areas. Where we will get to know the Unique Personalities of the different Cats To become the PURfect Cat Owner!" )
+# print( "The goal of the game is to adopt all of the 16 cats. Each correct action towards a cat earns you “Purr Points”. The more Purr Points you earn the higher probability to adopt the cat. You have nine lives and you will lose a life and 3 “Purr Points” if the cat chooses not to be adopted." )
+# print( "CATch them all! Hope you have PAWsome time playing our game!" )

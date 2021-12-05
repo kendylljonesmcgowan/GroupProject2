d059d6acfa0ea9bc3f78211257776ec019d2605b
valonte <vinceruel.alonte@flemingcollege.ca>
2021-12-05 01:11:05 -0500

    function

--- Vince.py
+++ Vince.py
@@ -1,1 +1,14 @@
-#test
+#Loki Function
+
+def lokipoint(lokilist):
+    lokicombination = ("Y", "Y", "Y", "Y", "N")
+    userlokicombination = lokilist
+    lokipoints = 0
+    for trait in range(5):
+        if lokicombination[trait] == userlokicombination[trait].upper():
+            lokipoints += 1
+    return lokipoints
+
+user = ["N","Y","N","Y","n"]
+point = lokipoint(user)
+print(point)

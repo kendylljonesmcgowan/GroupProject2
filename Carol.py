5fbe7fe40fd71ed53b6af93b2becd31dd2ea8b6d
Carolbuckingham <clebuckingham@gmail.com>
2021-12-05 15:41:08 -0500

    update Carol

--- Carol.py
+++ Carol.py
@@ -8,7 +8,7 @@ def main(username):
 
 # write up intros for each cat at Happy Tails Rescue: Gary, Buster, Ophelia, Artemis
 # each cat needs 8 statements for positive and negative interactions
-locations = ["A","B","C","D"]
+
 # location input exception:
 def main(location_exception):
   while current_location = chosen_location:
@@ -20,21 +20,20 @@ def main(location_exception):
 # define beginning variables for lives and points
 lives = 9
 purr_points = 0
-while lives > 0:
-  # CALL FULL MAIN GAME within this while statement
-# WITHIN WHILE LOOP: likely has to be defined and called after every full cat event
-  if lives = 0:
-    break # this will break the whole while loop onces lives have run out
 
-# end of FULL GAME LOOP due to loop break
+# WITHIN LOOP: likely has to be defined and called after every full cat event
 if lives = 0:
-  print("You've run out of lives!")
-  print("GAME OVER")
-  print("YOu adopted the following cats:"+ adopted_cats + "!")
-  print("Purr points collected:" + purr_points)
-else:
-  print("You adopted the following cats:"+ adopted_cats + "!")
-  print("Purr points collected:" + purr_points + ";  Lives remaining:" + lives)
+  break # this will break the whole game loop onces lives have run out
+# end of FULL GAME LOOP due to loop break
+def main(end_statements):
+  if lives = 0:
+    print("You've run out of lives!")
+    print("GAME OVER")
+    print("YOu adopted the following cats:"+ adopted_cats + "!")
+    print("Purr points collected:" + purr_points)
+  else:
+    print("You adopted the following cats:"+ adopted_cats + "!")
+    print("Purr points collected:" + purr_points + ";  Lives remaining:" + lives)
 
 # while loop for individual cat
 negative_interactions = 0
@@ -42,13 +41,19 @@ positive_interactions = 0
 while negative_interactions < 3:
   print(action_options)
   action = input("  What would you like to do wtih", current_cat, "? :  ")
-  if action = "N":
+  if action = False:
     negative_interactions+=1
     print("negative interaction statment for cat")
   else:
     positive_interactions+=1
     print("positive interaction statement for cat")
-    
+
+# negative interactions in 3rd and following interactions with cats within cat interaction events
+if negative_interactions == 3:
+  print("Oh meow!! This cat doesn't want to come home with you!")
+  lives-=1
+  continue     # within a loop
+
 
 # DON'T DELETE THIS
 if __name__=='__main__':  #calling defined function 'main
